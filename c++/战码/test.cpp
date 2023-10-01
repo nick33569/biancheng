09c070eb575f546bbe2c74acad3f02327731f6c1
nick33569 <nick3612@outlook.com>
2023-10-01 14:31:59 +0800

    c2

--- c++/战码/test.cpp
+++ c++/战码/test.cpp
@@ -1,40 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct node{
-	int value;
-	node *l,*r;
-	node(int value=value,node *l=NULL,node *r=NULL):value(value),l(l),r(r){};
-}
-void buildtree(int t,node *&root){
-	if(!root) root=new node;
-	else if(t<root->value) buildtree(t,root->l);
-	else buildtree(t,root->r);
-}
-void preorder(node *root){
-	if(root!=NULL){
-		printf("%d ",root->value);
-		preorder(root->l);
-		preorder(root->r);
-	}
-}
-void removetree(node *root){
-	if(!root) return;
-	removetree(root->l);
-	removetree(root->r);
-	delete root;
-}
 int main(){
-	int n,t=0;
-	
-	while(~scanf("%d",&n)){
-		node root=new node;
-		for(int i=0;i<n;i++){
-			cin>>t;
-			buildtree(t,root);
+	int n,x=1;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		for(int j=0;j<n;j++){
+			if(x<10) cout<<"0"<<x;
+			else cout<<x;
+			x++;
 		}
-		preorder(root);
-		removetree(root);
+		cout<<endl;
 	}
-    return 0;
+	cout<<endl;
+	x=1;int z=n-1;
+	for(int i=1;i<=n;i++){
+		for(int j=z;j>0;j--) cout<<"  ";
+		for(int j=z;j<n;j++){
+			if(x<10) cout<<"0"<<x;
+			else cout<<x;
+			x++;
+		}
+		z--;
+		cout<<endl;
+	}
+	return 0;
 }
