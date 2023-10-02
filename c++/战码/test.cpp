1debc48cfcd7125ecee370315f0abdaf275a1838
nick33569 <nick3612@outlook.com>
2023-10-02 09:06:07 +0800

    1

--- c++/战码/test.cpp
+++ c++/战码/test.cpp
@@ -1,28 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-	int n,x=1;
-	cin>>n;
-	for(int i=1;i<=n;i++){
-		for(int j=0;j<n;j++){
-			if(x<10) cout<<"0"<<x;
-			else cout<<x;
-			x++;
+int main() {
+    int n;
+    cin>>n;
+    for (int i=2; i<=n;i++){
+		if(n%i==0){ 
+			cout<<n/i;
+			break;
 		}
-		cout<<endl;
 	}
-	cout<<endl;
-	x=1;int z=n-1;
-	for(int i=1;i<=n;i++){
-		for(int j=z;j>0;j--) cout<<"  ";
-		for(int j=z;j<n;j++){
-			if(x<10) cout<<"0"<<x;
-			else cout<<x;
-			x++;
-		}
-		z--;
-		cout<<endl;
-	}
-	return 0;
+    return 0;
 }
