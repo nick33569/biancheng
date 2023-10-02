6d7f8042837c8c5420af0ae542fa598b03240b77
nick33569 <nick3612@outlook.com>
2023-10-02 16:11:48 +0800

    6

--- c++/战码/test2.cpp
+++ c++/战码/test2.cpp
@@ -4,11 +4,29 @@ struct node{
 	int sum;
 	int ai,bi;
 	bool operator<(const node &rhs)const{
-		
+		return sum>rhs.sum;
 	}
 };
-priority_queue<node> pq;
+int a[50005],b[50005];
+priority_queue<node,vector<node>,less<node> > pq;
 int main(){
-	
+	// freopen("nsum.in","r",stdin);
+	// freopen("nsum.out","w",stdout);
+	ios::sync_with_stdio(false);
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++) cin>>a[i];
+	for(int i=0;i<n;i++) cin>>b[i];
+	sort(a,a+n);
+	sort(b,b+n);
+	for(int i=0;i<n;i++){
+		pq.push({a[i]+b[0],i,0});
+	}//优先队列初始化
+	for(int i=0;i<n;i++){
+		cout<<pq.top().sum<<" ";
+		node t=pq.top();//把top存起来
+		pq.push({a[t.ai]+b[t.bi+1],t.ai,t.bi+1});
+		pq.pop();
+	}
     return 0;
 }
--- c++/战码/国庆节训练23/10.2/T4.cpp
+++ c++/战码/国庆节训练23/10.2/T4.cpp
@@ -1,25 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long a[50005],b[50005],c[2500000005];
-bool cmp(long long a,long long b){
-	return a<b;
-}
+struct node{
+	int sum;
+	int ai,bi;
+	bool operator<(const node &rhs)const{
+		return sum>rhs.sum;
+	}
+};
+int a[50005],b[50005];
+priority_queue<node,vector<node>,less<node> > pq;
 int main(){
 	freopen("nsum.in","r",stdin);
 	freopen("nsum.out","w",stdout);
-    int n,cnt=0;
+	ios::sync_with_stdio(false);
+	int n;
 	cin>>n;
-	for(int i=0;i<n;i++) scanf("%lld",&a[i]);
-	for(int i=0;i<n;i++) scanf("%lld",&b[i]);
-	sort(a,a+n,cmp);
-	sort(b,b+n,cmp);
-	
+	for(int i=0;i<n;i++) cin>>a[i];
+	for(int i=0;i<n;i++) cin>>b[i];
+	sort(a,a+n);
+	sort(b,b+n);
 	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			c[cnt++]=a[i]+b[j];
-		}
+		pq.push({a[i]+b[0],i,0});
+	}//优先队列初始化
+	for(int i=0;i<n;i++){
+		cout<<pq.top().sum<<" ";
+		node t=pq.top();//把top存起来
+		pq.push({a[t.ai]+b[t.bi+1],t.ai,t.bi+1});
+		pq.pop();
 	}
-	sort(c,c+cnt,cmp);
-	for(int i=0;i<n;i++) printf("%lld ",c[i]);
     return 0;
 }
