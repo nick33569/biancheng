821a24b7af3436f7274de7c5e64518a0f0433102
nick33569 <nick3612@outlook.com>
2023-11-26 11:04:47 +0800

    Update 临时.cpp

--- c++/战码/临时.cpp
+++ c++/战码/临时.cpp
@@ -1,3 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-//this is just a code!!!
+const int INF=1<<30;
+int a[105][105];
+int dp[105];
+int main(){
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++) cin>>a[i][j];
+	}
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++) cout<<a[i][j]<<" ";
+		cout<<endl;
+	}
+	dp[1]=a[1][1];
+	for(int i=1;i<=n;i++){
+		for(int j=1;i<=j;j++){
+			dp[i]=max(dp[i],dp[i]+(max(a[i+1][j],a[i+1][j+1])));
+			cout<<dp[i]<<endl;
+		}
+	}
+	cout<<dp[n];
+    return 0;
+}
